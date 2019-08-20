f23360dfd4e7d2f8c6a11af533042016a3d11394
Samuel <samuel.bagattin@gmail.com>
2019-08-20 19:57:27 +0200

    update .gitignore

--- publish.sh
+++ publish.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-
-dotnet publish -r linux-arm --configuration Release
-ssh pi@176.159.24.54 -p 52022 ' rm -rf ~/deployment && mkdir ~/deployment '
-scp -r -P 52022 ./bin/Release/netcoreapp2.2/linux-arm/publish/ pi@176.159.24.54:~/deployment/
-ssh pi@176.159.24.54 -p 52022 ' dotnet ~/deployment/BMP280API.dll '
+printf ' pass : '
+read pass
+printf  "$pass\n$pass\n$pass" | (
+dotnet publish -r linux-arm --configuration Release &&
+ssh pi@176.159.24.54 -p 52022 ' rm -rf ~/deployment && mkdir ~/deployment ' &&
+scp -r -P 52022 ./bin/Release/netcoreapp2.2/linux-arm/publish/. pi@176.159.24.54:~/deployment/ &&
+ssh pi@176.159.24.54 -p 52022 ' export DOTNET_ROOT=$HOME/dotnet &&
+export PATH=$PATH:$HOME/dotnet && nohup dotnet ~/deployment/publish/BMP280API.dll'
+)
