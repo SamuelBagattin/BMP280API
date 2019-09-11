5908ceaea47183f9400ef793f424bf2d68ac5952
Samuel Bagattin <42091952+SamuelBagattin@users.noreply.github.com>
2019-09-11 21:36:45 +0200

    fix url strings

--- Program.cs
+++ Program.cs
@@ -43,7 +43,7 @@ namespace BMP280API
                         config.AddCommandLine(args);
                     }                })
                 .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.0:5000; http://localhost:5000")
+                .UseUrls("http://0.0.0.0:5000;http://localhost:5000")
         ;
     }
 }
