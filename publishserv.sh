b50feb240b9844e91e71b5a6bed29577cde95c7f
Samuel Bagattin <42091952+SamuelBagattin@users.noreply.github.com>
2019-09-11 22:30:43 +0200

    Update deployment script

--- publishserv.sh
+++ publishserv.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-git pull --rebase &&
 dotnet restore &&
-rm -rf ~/deployment && mkdir ~/deployment &&
-dotnet publish -r linux-arm --configuration Release -o ../../deployment/ &&
+rm -rf ./build && mkdir ./build &&
+dotnet publish -r linux-arm --configuration Release -o ./build &&
 fuser -k 5000/tcp &&
 rm -rf nohup.out
 nohup dotnet ~/deployment/BMP280API.dll &
